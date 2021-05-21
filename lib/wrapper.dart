b1e18f64f8c2204591c9b3e8704f337a03697a69
gilangpersada <gilangpersadaa@gmail.com>
2021-05-21 23:15:01 +0700

    app signedout

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/auth_service.dart';
 import 'package:todo_app/login_page.dart';
 import 'package:todo_app/main_page.dart';
 
@@ -8,7 +9,7 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User firebaseUser = Provider.of<User>(context);
-
+    AuthService.signOut();
     if (firebaseUser == null) {
       return LoginPage();
     } else {
